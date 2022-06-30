1babb605014b9efaf36cd168c37dbc388890d424
guilhermecostam <guicm007@gmail.com>
2022-06-30 01:46:56 -0300

    feat: add 404 view page with styles now

--- views/error/404.php
+++ views/error/404.php
@@ -1,1 +1,17 @@
 <h1>404 - Página não encontrada</h1>
+
+<?php include __DIR__ . '/_partials/header.php'; ?>
+
+    <main>
+      <section class="s-hero">
+        <div class="container">
+          <div class="title-box">
+            <h2>
+                404 - <span class="name"> Page not found</span>
+            </h2>
+          </div>
+        </div>
+      </section>
+    </main>
+
+<?php include __DIR__ . '/_partials/footer.php'; ?>
