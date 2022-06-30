2fd09c657450dbd88de5f101bc2a00e4a2c8274c
guilhermecostam <guicm007@gmail.com>
2022-06-30 13:52:58 -0300

    feat: add phpunit in dependencies

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $response = [];
+        $this->assertEmpty($response);
+    }
+}
